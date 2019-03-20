b0d46128907764be4530a0f94981ec5fffd75e32
User <hunter9133@yahoo.com.tw>
2019-03-20 20:13:22 +0800

    login page

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+function postPage(){
+    $.post(page + '.html', {}, function (rt, callback) {
+        console.log(rt)
+        // loading(false);
+        // $('.main').html(rt).addClass('fadeIn');
+        // if ($.isFunction(callback)) callback();
+    })
+}
+postPage();
